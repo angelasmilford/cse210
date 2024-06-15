fe17bb2969a0efc764484a519343bcdbbcae4f2e
angelasmilford <angela.milford123@gmail.com>
2024-06-15 11:17:36 -1100

    Program Specification for Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Ask for first name
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+
+        //Ask for last name
+        Console.Write("What is your last name? ");
+        string surname = Console.ReadLine();
+
+        //Print first and last name
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {surname}, {name} {surname}.");
     }
 }
