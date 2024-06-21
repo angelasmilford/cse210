712a5abf4a29fcbf9355d0ed5d2e355b1a127d0e
angelasmilford <angela.milford123@gmail.com>
2024-06-20 13:53:01 -1100

    Contains the needed member variables and a single method for Job.

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,14 @@
+public class Job
+{
+    //state
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    //behavior
+    public void Display() //displays job details
+    {
+        Console.WriteLine($"{._jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
